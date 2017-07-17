e565857d9086f4a7e34fec906ac1c1bb3f3c4352
AmaliaRiveraC <amaliariveracastillejos@gmail.com>
2017-07-17 16:58:47 +0000

    repo init

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.use('/data', express.static(path.join(__dirname, 'data')));
+app.use('/static', express.static(path.join(__dirname, 'assets')));
+app.use('/static', express.static(path.join(__dirname, 'node_modules')));
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/index.html');
+	console.log('escuchando servidor 8000');
+});
+
+app.listen(8000);
